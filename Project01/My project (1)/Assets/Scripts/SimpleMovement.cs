05f81400455c0b8b81cf1f0b4cf5d8966670d697
MMahdi Jahangiri <117528215+devPirate01@users.noreply.github.com>
2026-09-09 11:43:15 +0200

    Added multiplayer and assets

--- Project01/My project (1)/Assets/Scripts/SimpleMovement.cs
+++ Project01/My project (1)/Assets/Scripts/SimpleMovement.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class SimpleMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float rotateSpeed = 120f;
+    public int playerIndex = 0; // 0 = WASD, 1 = Arrow Keys
+
     CharacterController cc;
     Animator anim;
-    Vector2 input;
 
     void Start()
     {
@@ -14,15 +17,29 @@ public class SimpleMovement : MonoBehaviour
         anim = GetComponentInChildren<Animator>();
     }
 
-    // Called automatically by PlayerInput component (Send Messages mode)
-    void OnMove(UnityEngine.InputSystem.InputValue v) => input = v.Get<Vector2>();
-
     void Update()
     {
-        Vector3 move = new Vector3(input.x, -1f, input.y);
+        var kb = Keyboard.current;
+        if (kb == null) return;
+
+        float forward, rotate;
+
+        if (playerIndex == 0)
+        {
+            forward = (kb.wKey.isPressed ? 1 : 0) - (kb.sKey.isPressed ? 1 : 0);
+            rotate  = (kb.dKey.isPressed ? 1 : 0) - (kb.aKey.isPressed ? 1 : 0);
+        }
+        else
+        {
+            forward = (kb.upArrowKey.isPressed ? 1 : 0) - (kb.downArrowKey.isPressed ? 1 : 0);
+            rotate  = (kb.rightArrowKey.isPressed ? 1 : 0) - (kb.leftArrowKey.isPressed ? 1 : 0);
+        }
+
+        // Rotate then move forward
+        transform.Rotate(0, rotate * rotateSpeed * Time.deltaTime, 0);
+        Vector3 move = transform.forward * forward + Vector3.down;
         cc.Move(move * speed * Time.deltaTime);
-        move.y = 0;
-        if (move.sqrMagnitude > 0) transform.forward = move;
-        if (anim) anim.SetFloat("Speed", input.magnitude);
+
+        if (anim) anim.SetFloat("Speed", Mathf.Abs(forward));
     }
 }
